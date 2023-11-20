dfd0dc5286c191d23d9297b406320e58b1354cab
JacobDev1 <contact@codepoems.eu>
2023-11-20 20:32:14 +0100

    Refactor SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,21 @@
 import { NavigateFunction } from "react-router";
 
-export default function SearchBar({navigate}:{navigate: NavigateFunction}){
-    function handleSubmit(e: any){
-        e.preventDefault();
-        if(e.target.phrase.value.replace(/ /g, "") == ""){return;}  // Prevent empty search
-        navigate(`/search?q=${e.target.phrase.value}`);
-    }
+interface SearchBarProps{
+    navigate: NavigateFunction;
+}
 
+export default function SearchBar({navigate}: SearchBarProps){
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        const input = (form.elements.namedItem("phrase") as HTMLInputElement).value.trim();
+    
+        if (input) {
+            navigate(`/search?q=${encodeURIComponent(input)}`);
+        }    
+      };
+    
     return(<form className="search" onSubmit={handleSubmit}>
         <input type="text" name="phrase" placeholder="Search..."/>
         {/* <button type="submit">Search</button> */}
