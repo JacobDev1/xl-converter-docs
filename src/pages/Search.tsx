2541ab931646750754754a6000722c41f1463a01
JacobDev1 <contact@codepoems.eu>
2023-11-21 14:04:01 +0100

    snake_case -> camelCase

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { clamp } from "../components/utils";
-import search_index from "../assets/search_index.json";
+import searchIndex from "../assets/search_index.json";
 import parse from "html-react-parser";
 
 interface SearchProps{
@@ -12,46 +12,48 @@ export default function Search({updateSelectedTab}: SearchProps){
     const [searchParams] = useSearchParams();
     const [entries, setEntries] = useState<string[][]>([]);
 
-    function insertPhrase(content: string, index: number, phrase_org: string, phrase_rep: string){
-        return content.substring(0, index) + phrase_rep + content.substring(index + phrase_org.length);
+    // Replace a specified phrase with a new phrase in a content
+    function insertPhrase(content: string, index: number, phraseOrg: string, phraseNew: string){
+        return content.substring(0, index) + phraseNew + content.substring(index + phraseOrg.length);
     }
 
-    function getDescription(content: string, phrase: string, phrase_idx: number){
-        const content_len = content.length;
+    // Return a description of a search entry
+    function getDescription(content: string, phrase: string, phraseIdx: number){
+        const contentLen = content.length;
 
         // Starting values
-        let from = clamp(phrase_idx - 70, 0, content_len);
-        let to = clamp(phrase_idx + 100, 0, content_len);
+        let from = clamp(phraseIdx - 70, 0, contentLen);
+        let to = clamp(phraseIdx + 100, 0, contentLen);
 
         // Display whole words
         while(content[from] != " " && from > 0){
             from--;
         }
 
-        while(content[to] != " " && to < content_len - 1){
+        while(content[to] != " " && to < contentLen - 1){
             to++;
         }
 
-        let new_content = content.substring(from, to);
-        const new_index = new_content.toLowerCase().indexOf(phrase.toLowerCase());
+        let newContent = content.substring(from, to);
+        const newIndex = newContent.toLowerCase().indexOf(phrase.toLowerCase());
 
-        // Mark searched phrase in bold
-        new_content = insertPhrase(new_content, new_index, phrase ,`<b class="highlight">${phrase}</b>`);
+        // Mark the searched phrase in bold
+        newContent = insertPhrase(newContent, newIndex, phrase ,`<b class="highlight">${phrase}</b>`);
         
-        return new_content;
+        return newContent;
     }
 
     useEffect(() => {
         let phrase = searchParams.get("q") as string;
-        let new_entries = search_index
+        let newEntries = searchIndex
             .filter((i) => i.content.toLowerCase().includes(phrase.toLowerCase()))
             .map((i) => {
                 const idx = i.content.toLowerCase().indexOf(phrase.toLowerCase());
-                const org_phrase = i.content.substring(idx, idx + phrase.length);
-                return [i.title, getDescription(i.content, org_phrase, idx), i.link];
+                const orgPhrase = i.content.substring(idx, idx + phrase.length);
+                return [i.title, getDescription(i.content, orgPhrase, idx), i.link];
             });
 
-        setEntries(new_entries);
+        setEntries(newEntries);
     }, [searchParams]);
 
     return(<div className="search">
