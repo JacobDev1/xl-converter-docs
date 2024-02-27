33de74a5addf60359ba95de0079015b36d7b1cd8
JacobDev1 <contact@codepoems.eu>
2024-02-27 17:40:52 +0100

    getting_started: Clean up

--- src/pages/GettingStarted.tsx
+++ src/pages/GettingStarted.tsx
@@ -11,12 +11,12 @@ export default function GettingStarted(){
         <p>Only download XL Converter from codepoems.eu.</p>
 
         <h3>Windows</h3>
-        <p>Double-click on the downloaded file. You will get a warning, click on</p>
+        <p>Double-click on the downloaded file, a warning may appear. Click on</p>
         <ul>
             <li>More Info (link)</li>
             <li>Run Anyway (button)</li>
         </ul>
-        <p>This happens for every program that hasn't rented an expensive digital signature, which serves no other purpose than to remove this popup. The Ardour developers put it nicely <a href="https://ardour.org/first_time_windows.html" target="_blank">into words</a>.</p>
+        <p>Windows shows this warning on all software that hasn't rented an expensive digital signature, which serves no other purpose than to remove this popup. This is a <a href="https://ardour.org/windows_defender.html" target="_blank">common problem</a>.</p>
 
         <h3>Linux</h3>
         <ul>
@@ -39,20 +39,14 @@ export default function GettingStarted(){
         </ul>
         <h3>Linux</h3>
         <h4>AVIF</h4>
-        <p>Install the package.</p>
+        <p>Install this package.</p>
         <p><code>sudo apt install libavif-gdk-pixbuf</code></p>
         <p><a href="https://codepoems.eu/posts/how-to-open-avif-images-on-linux/" target="_blank">Detailed tutorial</a></p>
         <h4>JPEG XL</h4>
-        <p>If you're security conscious, you might want to just stick with AVIF.</p>
+        <p>For security-conscious Linux users, sticking with AVIF may be preferable.</p>
         <p>Download the latest <a href="https://github.com/libjxl/libjxl/releases">binaries</a> for your distribution.</p>
         <p>Unpack then install them</p>
         <p><code>sudo dpkg -i *deb</code></p>
         <p><a href="https://codepoems.eu/posts/how-to-open-jpeg-xl-images-on-linux/" target="_blank">Detailed tutorial</a></p>
-
-        <h2>Updates</h2>
-        <p>Check for updates in the <b>About tab</b>.</p>
-
-        <h2>Is XL Converter Safe?</h2>
-        <p>The program is safe to use and the source code is publicly available.</p>
     </div>);
 }
